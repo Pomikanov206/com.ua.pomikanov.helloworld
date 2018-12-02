4f5138b67cc9abf05fe7f91b2da4fd7271b196e4
Pomikanov206 <pomikanov@gmail.com>
2018-12-03 00:49:54 +0200

    Update. Added Logger.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,8 +2,13 @@ import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HelloWorld {
+
+    private static Logger logger = Logger.getLogger(HelloWorld.class.getName());
+
     public void printMessage(Calendar date)  {
 
         ResourceBundle myBundle = ResourceBundle.getBundle("HelloWords");
@@ -11,18 +16,21 @@ public class HelloWorld {
         int hour = date.get(Calendar.HOUR_OF_DAY) + 1;
 
         try {
-
+            String resultLine = null;
             if (hour >= 6 && hour < 9)
-                System.out.println(new String(myBundle.getString("good_morning").getBytes("ISO8859-1")));
+                resultLine = new String(myBundle.getString("good_morning").getBytes("ISO8859-1"));
             else if (hour >= 9 && hour < 19)
-                System.out.println(new String(myBundle.getString("good_day").getBytes("ISO8859-1")));
+                resultLine = new String(myBundle.getString("good_day").getBytes("ISO8859-1"));
             else if (hour >= 19 && hour < 23)
-                System.out.println(new String(myBundle.getString("good_evening").getBytes("ISO8859-1")));
+                resultLine = new String(myBundle.getString("good_evening").getBytes("ISO8859-1"));
             else
-                System.out.println(new String(myBundle.getString("good_night").getBytes("ISO8859-1")));
+                resultLine = new String(myBundle.getString("good_night").getBytes("ISO8859-1"));
+
+            System.out.println(resultLine);
+            logger.info(resultLine);
 
         } catch (UnsupportedEncodingException e) {
-            System.out.println("Sorry. Encoding problems.");
+            logger.log(Level.WARNING,"Encoding problems.");
         }
     }
 
