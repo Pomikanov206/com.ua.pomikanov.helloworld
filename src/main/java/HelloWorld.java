fc5cfe05ddcb0c52284cbc3cfc42fd08270936a6
Pomikanov206 <pomikanov@gmail.com>
2018-12-03 00:29:48 +0200

    Added JUnit tests. And added russian properties encoding

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,26 +1,33 @@
+import java.io.UnsupportedEncodingException;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class HelloWorld {
-    public void printMessage(Calendar date, Locale locale){
+    public void printMessage(Calendar date)  {
 
-        ResourceBundle myBundle = ResourceBundle.getBundle("hello_world");
+        ResourceBundle myBundle = ResourceBundle.getBundle("HelloWords");
 
         int hour = date.get(Calendar.HOUR_OF_DAY) + 1;
-        if(hour >= 6 && hour < 9 )
-            System.out.println("Good morning, World!");
-        else if(hour >= 9 && hour < 19)
-            System.out.println("Good day, World!");
-        else if(hour >= 19 && hour < 23)
-            System.out.println("Good evening, World!");
-        else
-            System.out.println("Good night, World!");
-    }
-    public void printA(){
-        System.out.println("A");
-    }
-    public static void main(String[] args) {
 
+        try {
+
+            if (hour >= 6 && hour < 9)
+                System.out.println(new String(myBundle.getString("good_morning").getBytes("ISO8859-1")));
+            else if (hour >= 9 && hour < 19)
+                System.out.println(new String(myBundle.getString("good_day").getBytes("ISO8859-1")));
+            else if (hour >= 19 && hour < 23)
+                System.out.println(new String(myBundle.getString("good_evening").getBytes("ISO8859-1")));
+            else
+                System.out.println(new String(myBundle.getString("good_night").getBytes("ISO8859-1")));
+
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("Sorry. Encoding problems.");
+        }
+    }
+
+    public static void main(String[] args){
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printMessage(Calendar.getInstance());
     }
 }
